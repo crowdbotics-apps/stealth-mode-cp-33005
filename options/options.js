460dcc6ccf563a0905a6d34203a318e55d80962e
Crowdbotics <team@crowdbotics.com>
2022-01-21 15:56:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "stealth_mode_cp_33005",
+  url: "https://stealth_mode_cp_33005.botics.co",
+  api: "https://stealth_mode_cp_33005.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
